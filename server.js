ad749b3961f6499c889a7b5cdc09f761878514bb
Minjung Kim <fgh2018@naver.com>
2025-03-14 23:46:39 +0900

    bringing in CSS file in HTML in Node js

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
+app.use(express.static(__dirname + '/public'));
+
 app.listen(8080, () => {
     console.log("8080번 포트에서 서버 가동 중");
 })
